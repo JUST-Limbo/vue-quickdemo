58fa3ebb767549571f548ef99d0c28adb6e381c5
zp36523 <1034935762@qq.com>
2021-12-17 20:11:47 +0800

    2021年12月17日20:11:47

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VideoPlayer from 'vue-video-player'
+require('video.js/dist/video-js.css')
+require('vue-video-player/src/custom-theme.css')
+Vue.use(VideoPlayer)
+import hls from 'videojs-contrib-hls'
+//  const hls = require('videojs-contrib-hls')
+Vue.use(hls)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: (h) => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import test from '../views/test.vue'
+import father from '../views/father.vue'
+import player from '../views/player.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/father',
+		name: 'father',
+		component: father
+	},
+	{
+		path: '/player',
+		name: 'player',
+		component: player
+	},
+	{
+		path: '/test',
+		name: 'test',
+		component: test
+	},
+	{
+		path: '/about',
+		name: 'About',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/About.vue')
+	}
 ]
 
 const router = new VueRouter({
-  routes
+	routes
 })
 
 export default router
